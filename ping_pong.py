68a8444821c69a42716bd8c300b5758d198f2f2a
Sadgoose0 <lipatnikovg@internet.ru>
2025-05-04 17:05:10 +0300

    сделал фон
    сделал фон и создал классы двух ракеток

---
+++ ping_pong.py
@@ -0,0 +1,97 @@
+from pygame import *
+from random import randint
+from time import time as timer
+#подгружаем отдельно функции для работы со шрифтом
+font.init()
+font1 = font.SysFont('Arial', 80)
+win = font1.render('YOU WIN!', True, (255, 255, 255))
+lose = font1.render('YOU LOSE!', True, (180, 0, 0))
+
+font2 = font.SysFont('Arial', 36)
+
+#нам нужны такие картинки:
+img_hero = "rocket.png" #герой
+img_enemy = "ufo.png" #враг
+
+#класс-родитель для других спрайтов
+class GameSprite(sprite.Sprite):
+ #конструктор класса
+    def __init__(self, player_image, player_x, player_y, size_x, size_y, player_speed):
+        #вызываем конструктор класса (Sprite):
+        sprite.Sprite.__init__(self)
+ 
+ 
+        #каждый спрайт должен хранить свойство image - изображение
+        self.image = transform.scale(image.load(player_image), (size_x, size_y))
+        self.speed = player_speed
+ 
+ 
+        #каждый спрайт должен хранить свойство rect - прямоугольник, в который он вписан
+        self.rect = self.image.get_rect()
+        self.rect.x = player_x
+        self.rect.y = player_y
+ #метод, отрисовывающий героя на окне
+    def reset(self):
+        window.blit(self.image, (self.rect.x, self.rect.y))
+
+#класс главного игрока
+class Player(GameSprite):
+    #метод для управления спрайтом стрелками клавиатуры
+    def update(self):
+        keys = key.get_pressed()
+        if keys[K_LEFT] and self.rect.x > 5:
+            self.rect.x -= self.speed
+        if keys[K_RIGHT] and self.rect.x < win_width - 80:
+            self.rect.x += self.speed
+
+class Enemy(GameSprite):
+    #движение врага
+    def update(self):
+        self.rect.y += self.speed
+        global lost
+        #исчезает, если дойдёт до края экрана
+        if self.rect.y > win_height:
+            self.rect.x = randint(80, win_width - 80)
+            self.rect.y = 0
+
+
+win_width = 700
+win_height = 500
+display.set_caption("Ping-pong")
+window = display.set_mode((win_width, win_height))
+window.fill((255, 0, 70))
+
+class Racket_1(GameSprite):
+    def update(self):
+        keys = key.get_pressed()
+        if keys[K_W] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[K_S] and self.rect.y < win_height - 80:
+            self.rect.y += self.speed
+
+class Racket_2(GameSprite):
+    def update(self):
+        keys = key.get_pressed()
+        if keys[K_UP] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[K_DOWN] and self.rect.y < win_height - 80:
+            self.rect.y += self.speed
+
+#переменная "игра закончилась": как только там True, в основном цикле перестают работать спрайты
+finish = False
+#основной цикл игры:
+run = True #флаг сбрасывается кнопкой закрытия окна
+clock = time.Clock()
+while run:
+    #событие нажатия на кнопку Закрыть
+    for e in event.get():
+        if e.type == QUIT:
+            run = False
+            
+ #сама игра: действия спрайтов, проверка правил игры, перерисовка
+    if not finish:
+        #обновляем фон
+        window.fill((255, 0, 70))
+
+        clock.tick(40)
+        display.update()
